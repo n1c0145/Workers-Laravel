67a9d3dba972405e26c6544676283d83cd298f83
n1c0145 <nicolas.lozalml@gmail.com>
2022-08-10 11:32:16 -0500

    List empleados method

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,29 @@
 <h1>Mostrar lista empleados</h1>
+
+<table class="table table-light">
+
+<thead class="thread-light">
+    <tr>
+        <th>#</th>
+        <th>Foto</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Correo</th>
+        <th>Acciones</th>
+    </tr>
+</thead>
+
+ <tbody>
+    @foreach( $empleados as $empleado )
+    <tr>
+<td>{{ $empleado->id }}</td>
+<td>{{ $empleado->Foto }}</td>
+<td>{{ $empleado->Nombre }}</td>
+<td>{{ $empleado->Apellido }}</td>
+<td>{{ $empleado->Correo }}</td>
+<td>Editar y Borrar</td>
+    </tr>
+    @endforeach
+</tbody> 
+
+</table>
