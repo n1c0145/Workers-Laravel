15aabe9d2c701bc967a7f207811577fdd86b5d66
n1c0145 <nicolas.lozalml@gmail.com>
2022-08-11 09:46:34 -0500

    Integrate laravel/ui bootstrap and auth

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,10 @@ Route::get('/', function () {
 //Route::get('/empleado/create', [EmpleadoController::class,'create']);
 
 Route::resource('empleado', EmpleadoController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
