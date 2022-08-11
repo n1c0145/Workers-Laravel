caf71e00f68972793ecb79997fb84365af16b771
n1c0145 <nicolas.lozalml@gmail.com>
2022-08-11 09:14:43 -0500

    Fix form with null data

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,17 +1,19 @@
 <div>
     <label for="Nombre">Nombre</label>   
-    <input type="text" name="Nombre" value="{{$empleado->Nombre}}" id="Nombre">
+    <input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre">
 </div>
 <div><label for="Apellido">Apellido</label>  
-<input type="text" name="Apellido" value="{{$empleado->Apellido}}" id="Apellido">
+<input type="text" name="Apellido" value="{{ isset($empleado->Apellido)?$empleado->Apellido:''}}" id="Apellido">
 </div>
 <div>
 <label for="Correo">Correo</label>   
-<input type="text" name="Correo" value="{{$empleado->Correo}}" id="Correo">
+<input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:'' }}" id="Correo">
 </div>
 <div>
 <label for="Foto">Foto</label>   
+@if(isset($empleado->Foto))
 <img src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt=""> 
+@endif
 <input type="file" name="Foto" value="" id="Foto"> 
 </div>
 <div>
