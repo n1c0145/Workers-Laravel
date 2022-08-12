c48b0f09c870ae761b87cdbd89ef43ffdd9e0a45
n1c0145 <nicolas.lozalml@gmail.com>
2022-08-12 13:07:02 -0500

    Add pagination

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -15,7 +15,7 @@ class EmpleadoController extends Controller
      */
     public function index()
     {
-        $datos['empleados']=Empleado::paginate(5);
+        $datos['empleados']=Empleado::paginate(1);
         return view('empleado.index', $datos);
     }
 
